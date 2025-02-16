2023f1efc580963a3801848c4c8ce8cf7bf41722
nalydrey <nalydrey@gmail.com>
2025-02-16 23:24:29 +0200

    retrive data

--- adxl345.c
+++ adxl345.c
@@ -7,8 +7,6 @@
 #include "adxl345.h"
 
 
-
-
 ADXL345 ADXL345_SPI_init(ADXL345_Init_struct * initData){
 
 	ADXL345 device;
@@ -32,10 +30,10 @@ ADXL345 ADXL345_SPI_init(ADXL345_Init_struct * initData){
 
 	HAL_SPI_Init(&device.SPI_Interface);
 
-	__adxl345_Init_SPI_GPIO(initData->MISO_port, initData->MISO_Pin, initData->interface_instance, MAIN);
-	__adxl345_Init_SPI_GPIO(initData->MOSI_port, initData->MOSI_Pin, initData->interface_instance, MAIN);
-	__adxl345_Init_SPI_GPIO(initData->SCK_port, initData->SCK_Pin, initData->interface_instance,MAIN);
-	__adxl345_Init_SPI_GPIO(initData->CS_port, initData->CS_Pin, initData->interface_instance, CS);
+	__ADXL345_Init_SPI_GPIO(initData->MISO_port, initData->MISO_Pin, initData->interface_instance, MAIN);
+	__ADXL345_Init_SPI_GPIO(initData->MOSI_port, initData->MOSI_Pin, initData->interface_instance, MAIN);
+	__ADXL345_Init_SPI_GPIO(initData->SCK_port, initData->SCK_Pin, initData->interface_instance,MAIN);
+	__ADXL345_Init_SPI_GPIO(initData->CS_port, initData->CS_Pin, initData->interface_instance, CS);
 
 	HAL_GPIO_WritePin(initData->CS_port, initData->CS_Pin, GPIO_PIN_SET);
 
@@ -45,7 +43,7 @@ ADXL345 ADXL345_SPI_init(ADXL345_Init_struct * initData){
 uint8_t ADXL345_ReadRegister(ADXL345 * device, uint8_t registerAddress){
 
 	uint8_t firstByte = READ_SINGLE_DATA | registerAddress;
-	uint8_t message[2] = {firstByte, 0xFF};
+	uint8_t message[2] = {firstByte, EMPTY_BYTE};
 	uint8_t tempBuffer[2];
 
 	HAL_GPIO_WritePin(device->CS_port, device->CS_Pin, GPIO_PIN_RESET);
@@ -55,7 +53,7 @@ uint8_t ADXL345_ReadRegister(ADXL345 * device, uint8_t registerAddress){
 	return tempBuffer[1];
 }
 
-void ADXL345_WriteRegister(ADXL345 * device, uint8_t registerAddress, uint8_t byte){
+void ADXL345_SetRegister(ADXL345 * device, uint8_t registerAddress, uint8_t byte){
 
 	uint8_t firstByte = WRITE_SINGLE_DATA | registerAddress;
 	uint8_t message[2] = {firstByte, byte};
@@ -65,16 +63,54 @@ void ADXL345_WriteRegister(ADXL345 * device, uint8_t registerAddress, uint8_t by
 	HAL_GPIO_WritePin(device->CS_port, device->CS_Pin, GPIO_PIN_SET);
 }
 
-uint16_t ADXL345_Read_X_axis(ADXL345 * device){
+ADXL345_Data ADXL345_Read_X_axis(ADXL345 * device){
 
+	uint8_t* tempBuffer = __ADXL345_Read_Registers(device, DATAX0, 2);
+
+	device->data.x.HighByte = tempBuffer[2];
+	device->data.x.LowByte = tempBuffer[1];
+
+	free(tempBuffer);
+
+	return device->data.x;
 }
 
-uint16_t ADXL345_Read_Y_axis(ADXL345 * device){
+ADXL345_Data ADXL345_Read_Y_axis(ADXL345 * device){
 
+	uint8_t* tempBuffer = __ADXL345_Read_Registers(device, DATAY0, 2);
+
+	device->data.y.HighByte = tempBuffer[2];
+	device->data.y.LowByte = tempBuffer[1];
+
+	free(tempBuffer);
+
+	return device->data.y;
 }
 
-uint16_t ADXL345_Read_Z_axis(ADXL345 * device){
+ADXL345_Data ADXL345_Read_Z_axis(ADXL345 * device){
+	uint8_t* tempBuffer = __ADXL345_Read_Registers(device, DATAZ0, 2);
 
+	device->data.z.HighByte = tempBuffer[2];
+	device->data.z.LowByte = tempBuffer[1];
+
+	free(tempBuffer);
+
+	return device->data.z;
+}
+
+ADXL345_AXIS ADXL345_Read_All_axes(ADXL345 * device){
+	uint8_t* tempBuffer = __ADXL345_Read_Registers(device, DATAX0, 6);
+
+	device->data.x.HighByte = tempBuffer[2];
+	device->data.x.LowByte = tempBuffer[1];
+	device->data.y.HighByte = tempBuffer[4];
+	device->data.y.LowByte = tempBuffer[3];
+	device->data.z.HighByte = tempBuffer[6];
+	device->data.z.LowByte = tempBuffer[5];
+
+	free(tempBuffer);
+
+	return device->data;
 }
 
 void ADXL345_Start_measurement(ADXL345 * device){
@@ -83,10 +119,10 @@ void ADXL345_Start_measurement(ADXL345 * device){
 
 	tempBuffer[0] = ADXL345_ReadRegister(device, POWER_CTL);
 
-	ADXL345_WriteRegister(device, POWER_CTL, tempBuffer[0] | 0x08);
+	ADXL345_SetRegister(device, POWER_CTL, tempBuffer[0] | 0x08);
 }
 
-void __adxl345_Init_SPI_GPIO(GPIO_TypeDef  *GPIOx, uint16_t pin, SPI_TypeDef * spi_instance, uint8_t mode){
+void __ADXL345_Init_SPI_GPIO(GPIO_TypeDef  *GPIOx, uint16_t pin, SPI_TypeDef * spi_instance, uint8_t mode){
 
 	GPIO_InitTypeDef gpioInit;
 
@@ -102,6 +138,70 @@ void __adxl345_Init_SPI_GPIO(GPIO_TypeDef  *GPIOx, uint16_t pin, SPI_TypeDef * s
 
 	HAL_GPIO_Init(GPIOx, &gpioInit);
 }
+
+
+int16_t ADXL345_Convert_Axis(ADXL345_Data axis){
+
+	uint8_t tempBuffer[2] = {axis.HighByte, axis.LowByte};
+
+	uint8_t isNegative = axis.HighByte>>7;
+
+		if(isNegative){
+			tempBuffer[0] = ~tempBuffer[0];
+			tempBuffer[1] = ~tempBuffer[1];
+		}
+
+	int16_t value = ((tempBuffer[0] << 8) | tempBuffer[1]);
+
+	return isNegative ? (-1)*value : value;
+}
+
+ADXL345_Values ADXL_345_Convert_Axes(ADXL345_AXIS axes){
+	ADXL345_Values tempAxes;
+	tempAxes.x = ADXL345_Convert_Axis(axes.x);
+	tempAxes.y = ADXL345_Convert_Axis(axes.y);
+	tempAxes.z = ADXL345_Convert_Axis(axes.z);
+
+	return tempAxes;
+}
+
+ADXL345_Values ADXL_345_Convert_Me(ADXL345 * adxl){
+	ADXL345_Values tempAxes;
+	tempAxes.x = ADXL345_Convert_Axis(adxl->data.x);
+	tempAxes.y = ADXL345_Convert_Axis(adxl->data.y);
+	tempAxes.z = ADXL345_Convert_Axis(adxl->data.z);
+
+	return tempAxes;
+}
+
+
+// !!! Important !!! When use this function don't forget to apply free();
+uint8_t* __ADXL345_Read_Registers(ADXL345 * device, uint8_t registerAddress, uint8_t size){
+
+	uint8_t localSize = size+1;
+
+	uint8_t firstByte = READ_MULTIPLE_DATA | registerAddress;
+
+	uint8_t* message = malloc(localSize * sizeof(uint8_t));
+
+	message[0] = firstByte;
+	for(uint8_t i = 1; i < localSize; i++){
+		message[i] = EMPTY_BYTE;
+	}
+
+	uint8_t* tempBuffer = malloc(localSize * sizeof(uint8_t));
+
+
+	HAL_GPIO_WritePin(device->CS_port, device->CS_Pin, GPIO_PIN_RESET);
+	HAL_SPI_TransmitReceive(&device->SPI_Interface, message, tempBuffer, localSize, HAL_MAX_DELAY);
+	HAL_GPIO_WritePin(device->CS_port, device->CS_Pin, GPIO_PIN_SET);
+
+	free(message);
+
+	return tempBuffer;
+}
+
+
 
 
 
--- adxl345.h
+++ adxl345.h
@@ -7,8 +7,12 @@
 
 #ifndef INC_ADXL345_H_
 #define INC_ADXL345_H_
+
 #include "main.h"
 #include "app_utils.h"
+#include <stdlib.h>
+
+#define EMPTY_BYTE 0xFF
 
 enum registers {
 	DEVID, 					// Device ID
@@ -45,7 +49,7 @@ enum registers {
 
 enum read_write_mode {
 	READ_SINGLE_DATA = 1<<7,
-	READ_MULTIPLE_DATA = 1<<0x03,
+	READ_MULTIPLE_DATA = (1<<6) | (1<<7),
 	WRITE_SINGLE_DATA = 0,
 	WRITE_MULTIPLE_DATA = 1<<6
 };
@@ -56,6 +60,18 @@ enum modes{
 };
 
 typedef struct {
+	uint8_t LowByte;
+	uint8_t HighByte;
+} ADXL345_Data;
+
+typedef struct {
+	ADXL345_Data x;
+	ADXL345_Data y;
+	ADXL345_Data z;
+} ADXL345_AXIS;
+
+typedef struct {
+	ADXL345_AXIS data;
 	uint8_t rxDataBuffer[10];
 	GPIO_TypeDef * CS_port;
 	uint16_t CS_Pin;
@@ -74,14 +90,24 @@ typedef struct {
 	SPI_TypeDef * interface_instance;
 } ADXL345_Init_struct;
 
+typedef struct {
+	int16_t x;
+	int16_t y;
+	int16_t z;
+} ADXL345_Values;
 
 
-void __adxl345_Init_SPI_GPIO(GPIO_TypeDef  *GPIOx, uint16_t pin, SPI_TypeDef * spi_instance, uint8_t mode);
+
+
+void __ADXL345_Init_SPI_GPIO(GPIO_TypeDef  *GPIOx, uint16_t pin, SPI_TypeDef * spi_instance, uint8_t mode);
+uint8_t* __ADXL345_Read_Registers(ADXL345 * device, uint8_t registerAddress, uint8_t size);
 ADXL345 ADXL345_SPI_init(ADXL345_Init_struct * initData);
 uint8_t ADXL345_ReadRegister(ADXL345 * device, uint8_t registerAddress);
-void ADXL345_WriteRegister(ADXL345 * device, uint8_t registerAddress, uint8_t byte);
+void ADXL345_SetRegister(ADXL345 * device, uint8_t registerAddress, uint8_t byte);
 void ADXL345_Start_measurement(ADXL345 * device);
-uint16_t ADXL345_Read_X_axis(ADXL345 * device);
-uint16_t ADXL345_Read_Y_axis(ADXL345 * device);
-uint16_t ADXL345_Read_Z_axis(ADXL345 * device);
+ADXL345_Data ADXL345_Read_X_axis(ADXL345 * device);
+ADXL345_Data ADXL345_Read_Y_axis(ADXL345 * device);
+ADXL345_Data ADXL345_Read_Z_axis(ADXL345 * device);
+int16_t ADXL345_Convert_Axis(ADXL345_Data axis);
+ADXL345_AXIS ADXL345_Read_All_axes(ADXL345 * device);
 #endif /* INC_ADXL345_H_ */
